791a32c7843e7182f350721e54234551ab690683
wangxiaolei@happyplaygame.net <wangxiaolei@happyplaygame.net>
2019-11-11 19:47:30 +0800

    port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/post', postRouter);
 app.use('/likecomment', likecommentRouter);
